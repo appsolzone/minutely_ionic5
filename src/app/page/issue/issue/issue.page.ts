6f796f7f926468ac9302bf593535b641143cadf1
uribbas <uribbas@gmail.com>
2021-04-09 07:36:54 +0530

    First commit for issue

---
+++ src/app/page/issue/issue/issue.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-issue',
+  templateUrl: './issue.page.html',
+  styleUrls: ['./issue.page.scss'],
+})
+export class IssuePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
